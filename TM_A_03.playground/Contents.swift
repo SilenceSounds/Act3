7263a9dd55a57eba72e3ebd879d32427c664639c
SilenceSounds <70463457+SilenceSounds@users.noreply.github.com>
2021-02-17 18:06:13 -0800

    Add files via upload

---
+++ TM_A_03.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+/*:
+# Playground - Actividad 3
+* Tipos de datos
+* Asociación de tipos
+* Arreglos y Diccionarios
+*/
+
+
+/*: 
+### Actividad de Tipos de datos
+A) Declarar cuatro variables con tres diferentes tipos de datos, cada uno que corresponda a una numero entero, un numero decimal (flotante), una cadena de texto, realizando la asignación explicita y la asignación inferida
+*/
+var Entero: Int = Int ()
+var Flotante: Float = Float()
+var Cadena: String = String()
+var CadenaInf = "Inferida"
+/*:
+### Asociación de tipos
+A) Declara el tipo de dato por asociación para un tipo de dato String
+*/
+var Astring = "Cadena de texto"
+//: B) Declara el tipo de dato por asociación para un tipo de dato  Número entero.
+var AInt = 5
+/*: 
+### Arreglos y Diccionarios
+A) Crea la variable "numeros" de tipo Array con números consecutivos del 1 a 10.
+*/
+var arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//: B) Crea la variable "diasSemana" de tipo Dictionary con la relación numero:día Ej. 1:"Lunes"
+var diasSemana = ["Lunes":1, "Martes":2, "Miercoles":3, "Jueves":4, "Viernes":5, "Sabado":6, "Domingo":7]
+
